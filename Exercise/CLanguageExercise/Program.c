b5d97b8c040650e58608155d36d406729feae874
kuranadomoe <Wearestardust404@outlook.com>
2017-02-15 17:37:16 +0800

    clear

--- Exercise/CLanguageExercise/Program.c
+++ Exercise/CLanguageExercise/Program.c
@@ -1,28 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 
 int main(int argc, char*argv[])
 {
-	int gold = 1000;
-	for (int flag = 0;!flag;++gold)
-	{
-		for (int i = 1,temp = gold;i <= 5;++i)
-		{
-			if (!(temp * 5 / 4 % 5))
-				temp = temp * 5 / 4 - 1;
-			else
-				break;
-			if (i == 5)
-			{
-				flag = 1;
-				gold = temp - 1;
-			}
-		}
-	}
-	fprintf(stdout, "gold total:%d\n", gold);
+
 	system("pause");
 	return 0;
 }
