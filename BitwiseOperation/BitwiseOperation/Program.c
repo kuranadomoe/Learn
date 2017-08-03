9434fe850ef3ce4c34f11f6b64d517e66393047d
kuranadomoe <Wearestardust404@outlook.com>
2017-08-03 15:05:18 +0800

    练习位运算

---
+++ BitwiseOperation/BitwiseOperation/Program.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int add(int a, int b);
+int sub(int a, int b);
+
+
+int main(int argc, char*argv[])
+{
+	int a = 0, b = 0;
+	fscanf(stdin, "%d %d", &a, &b);
+	fprintf(stdout, "%d + %d = %d\n", a, b, add(a, b));
+	fprintf(stdout, "%d - %d = %d\n", a, b, sub(a, b));
+	system("pause");
+	return 0;
+}
+
+
+int add(int a, int b)
+{
+	int result = a, carry = b;
+	for (int temp = result;carry;)
+	{
+		result ^= carry;
+		carry = (temp&carry) << 1;
+		temp = result;
+	}
+	return result;
+}
+
+
+int sub(int a, int b)
+{
+	int result = a, carry = b;
+	for (;carry;)
+	{
+		result ^= carry;
+		carry = (result&carry) << 1;
+	}
+	return result;
+}
