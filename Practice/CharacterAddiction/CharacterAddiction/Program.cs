189a1f098b44b4775ff28c850a180853249c9fd4
kuranadomoe <Wearestardust404@outlook.com>
2019-04-13 23:53:10 +0800

    sleep

---
+++ Practice/CharacterAddiction/CharacterAddiction/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading;
+using static System.Console;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace kuranado.moe.Practice.CharacterAddiction
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            WriteLine("Please input expression:");
+            List<string> input = new List<string>();
+            Regex regex = new Regex(@"^(?:[+]?[a-z A-Z]+|[-]+)$");
+            for (var line = ReadLine(); regex.IsMatch(line); line = ReadLine())
+                input.Add(line);
+            if (input.Count < 3)
+            {
+                WriteLine("Too few parameters!");
+                Pause();
+                Process.GetCurrentProcess().Kill();
+            }
+
+
+
+            Pause();
+        }
+
+        /// <summary>
+        /// Pause
+        /// </summary>
+        private static void Pause()
+        {
+            WriteLine("Press any key to continue...");
+            ReadKey(true);
+        }
+    }
+}
